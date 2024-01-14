bb1d7712c7193c7618dbadc45c78780f5778bfd6
adem <ademalagoz46@gmail.com>
2024-01-14 18:56:56 +0000

    post created

---
+++ src/app/1-props-example/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PostList from "@/component/PostList/PostList";
+import Navbar from "@/component/navbar/Navbar";
+
+const PropsExample = () => {
+  return (
+    <div>
+      <Navbar />
+      <PostList />
+    </div>
+  );
+};
+
+export default PropsExample;
