9cbd181df4fc88b0648953fedbc2291b8a747cfd
adem <ademalagoz46@gmail.com>
2024-07-18 23:39:08 +0100

    console cleaned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  console.log("hello");
   return <main className={styles.main}></main>;
 }
